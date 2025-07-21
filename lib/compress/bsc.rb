1f0f65eafd032c5adffde8dc3f888a49890eed64
Daniel Berger <78529+djberg96@users.noreply.github.com>
2025-07-21 18:12:31 -0400

    Yield self if block given.

--- lib/compress/bsc.rb
+++ lib/compress/bsc.rb
@@ -7,10 +7,32 @@ require_relative 'bsc/error'
 
 module Compress
   class BSC
-    # Initialize the BSC library with configurable features
+    # Initialize the BSC library with configurable features.
+    #
+    # This should always be called before attempting to use a compress
+    # or decompress operation.
+    #
+    # Yields itself if a block is given.
+    #
+    # Example:
+    #
+    #   require 'compress/bsc'
+    #
+    #   bsc = Compress::BSC.new
+    #   compressed_data = bsc.compress(something)
+    #   uncompressed_data = bsc.uncompress(compressed_data)
+    #
+    #   # or
+    #
+    #   Compress::BSC.new do |bsc|
+    #     compressed_data = bsc.compress(something)
+    #     uncompressed_data = bsc.uncompress(compressed_data)
+    #   end
+    #
     def initialize(features: Library::LIBBSC_DEFAULT_FEATURES)
       result = Library.bsc_init(features)
       raise Error.new(result) unless result == Library::LIBBSC_NO_ERROR
+      yield self if block_given?
     end
 
     # Simple compression interface
